2a25eb3bb9d2a9df74e4d883823d4a1d7aba75b1
Nicholas Kennedy <nmk10062@gmail.com>
2025-09-11 13:58:40 -0400

    Add image for bird, implement flapping and rotating when flying

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ PIPE_WIDTH = 60
 FONT_SMALL = pygame.font.SysFont("Verdana", 16)
 FONT_LARGE = pygame.font.SysFont("Verdana", 60)
 
+IMAGE_SCALE_FACTOR = 0.25
+
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), SCALED | DOUBLEBUF, vsync=1)
 pygame.display.set_caption('Flappy Bird')
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
+from pygame.math import clamp
 
-from main import RED, SCREEN_WIDTH, SCREEN_HEIGHT
+from main import RED, SCREEN_WIDTH, SCREEN_HEIGHT, IMAGE_SCALE_FACTOR
 from pygame.locals import *
 
 
@@ -8,10 +9,13 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
+        self.bird_image = pygame.transform.scale_by(pygame.image.load('images/bird.png').convert_alpha(), IMAGE_SCALE_FACTOR)
+        self.bird_flap_image = pygame.transform.scale_by(pygame.image.load('images/bird-flap.png').convert_alpha(), IMAGE_SCALE_FACTOR)
+        self.image = self.bird_image
         self.gravity = 1400
         self.velocity_y = 0
         self.size = 20
-        self.rect = pygame.Rect(0, 0, self.size, self.size)
+        self.rect = self.image.get_rect()
         self.rect.center = (SCREEN_WIDTH // 4, SCREEN_HEIGHT // 2)
         self.jump_power = 550
         self.jump_cooldown_ms = 225
@@ -27,12 +31,19 @@ class Player(pygame.sprite.Sprite):
             self.last_jump_time = ticks
             self.has_jumped = True
 
+        # show flap image when the bird is moving up
+        if self.velocity_y < 0:
+            self.image = self.bird_flap_image
+        else:
+            self.image = self.bird_image
+
         if self.has_jumped:
             self.velocity_y += self.gravity * deltatime
             self.rect.top = max(0, self.rect.top + self.velocity_y * deltatime)
 
     def draw(self, surface):
-        pygame.draw.rect(surface, RED, self.rect)
+        rotated_image = pygame.transform.rotate(self.image, clamp(-self.velocity_y / 20, -45, 45))
+        surface.blit(rotated_image, self.rect)
 
     def is_off_screen(self):
         return self.rect.bottom > SCREEN_HEIGHT
